e1b452d6cb79337c79d75529a2e62a5477b4fdd7
luxquinha <lucaxigomes@alu.ufc.br>
2024-05-15 12:14:12 -0300

    Instalando tailwindcss e react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Iniciando a aplicação</h1>
+      <span className="text-xl text-blue-400 font-bold">Iniciando a aplicação</span>
     </div>
   );
 }
