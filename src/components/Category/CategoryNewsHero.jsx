922f96399f91155cf34b643e37e500b97f384e1f
luxquinha <lucaxigomes@alu.ufc.br>
2024-05-26 16:11:36 -0300

    Esboço da AboutPage

--- src/components/Category/CategoryNewsHero.jsx
+++ src/components/Category/CategoryNewsHero.jsx
@@ -12,15 +12,14 @@ const CategoryNewsHero = ({mainNews}) => {
   return (
     <div className="flex flex-row flex-wrap px-4 h-[90%] gap-x-2 mt-2">
         {/* Card principal */}
+        <h2 className='font-medium text-2xl md:text-3xl text-skin-base hover:text-skin-base-hover lg:hidden text-justify indent-3 w-[90%] mb-2'>{mainNews[0]?.title}</h2>
         <Card.Root className='flex-grow basis-[49%] h-[320px] lg:h-[570px] shrink flex-col hover:cursor-default bg-transparent'>
           <Card.Content 
           title={mainNews[0]?.title} 
-          description={mainNews[0]?.description} 
-          className='z-10 px-3 h-1/5'
-          titleStyle='text-2xl md:text-3xl absolute top-0 w-[90%] text-skin-base hover:text-skin-base-hover lg:text-skin-inverted lg:hover:text-skin-inverted-hover lg:static'
-          descriptionStyle='hidden lg:flex'
+          className='z-10 px-3 h-auto text-ellipsis'
+          titleStyle='hidden lg:static lg:text-skin-inverted lg:hover:text-skin-inverted-hover lg:static'
           />
-          <Card.Image url={mainNews[0]?.urlToImage} className='absolute rounded-lg lg:hover:scale-110 duration-1000 lg:h-full'/>
+          <Card.Image url={mainNews[0]?.urlToImage} className='absolute rounded-lg lg:hover:scale-110 duration-1000 h-full'/>
           <Modal.Trigger action={()=>(handleModal(mainNews[0]))} icon={<Eye size={18}/>} className='right-2 top-2 w-6 h-6'/>
         </Card.Root>
         {/* Cards secundários */}
@@ -31,10 +30,8 @@ const CategoryNewsHero = ({mainNews}) => {
                 <Card.Image url={n.urlToImage} className='w-1/3 md:w-1/4 h-full'/>
                 <Card.Content 
                 title={n.title}
-                description={n.description}
                 className='bg-gradient-to-tr from-transparent to-transparent w-3/4 px-3 mt-2'
-                titleStyle='text-xl'
-                descriptionStyle='text-sm h-11 text-skin-base font-medium'
+                titleStyle='text-xl text-justify indent-2 pt-1'
                 />
                 <Modal.Trigger action={()=>(handleModal(n))} icon={<Eye size={16}/>} className='right-2 top-1 hover:bg-skin-primary hover:shadow-md bg-transparent w-6 h-6'/>
             </Card.Root>
